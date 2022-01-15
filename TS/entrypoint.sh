b47c7182bd064328cbdd3ad1fa3fc0303e143b5f
Samuel Eko <eko.5samuel@gmail.com>
2022-01-15 12:56:06 -0600

    add listen images

--- TS/entrypoint.sh
+++ TS/entrypoint.sh
@@ -3,4 +3,4 @@
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-gunicorn --bind 0.0.0.0:8000 techsemester.wsgi:application
+gunicorn --bind 0.0.0.0:9001 techsemester.wsgi:application
