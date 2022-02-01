73abd927d0685e068d42e0cf05a80a2ff056b11c
Samuel Eko <eko.5samuel@gmail.com>
2022-01-31 23:14:00 -0600

    black terminal

--- TS/entrypoint.sh
+++ TS/entrypoint.sh
@@ -3,4 +3,4 @@
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-uwsgi --socket :8008 --workers 4 --master --enable-threads --module techsemester.wsgi
+uwsgi --socket :9001 --workers 4 --master --enable-threads --module techsemester.wsgi
